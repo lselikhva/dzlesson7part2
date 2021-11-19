1e7344bcf086ee7e734c6404cf70a244748eca76
lselikhva <lselikhva@outlook.com>
2021-11-19 22:03:46 +0300

    package com.company;
    public class Main {
    
        public static void main(String[] args) {
            char[] reverseFullName = {'n', 'a', 'v', 'I', 'v', 'o', 'n', 'a', 'v', 'I'};
            for (int i = reverseFullName.length-1; i >= 0; i--){
                System.out.println("i = " + reverseFullName[i]);
    
            }
        }
    }
    "C:\Users\Asus GL752\.jdks\openjdk-17.0.1\bin\java.exe" "-javaagent:C:\Users\Asus GL752\AppData\Local\JetBrains\Toolbox\apps\IDEA-U\ch-0\212.5457.46\lib\idea_rt.jar=58717:C:\Users\Asus GL752\AppData\Local\JetBrains\Toolbox\apps\IDEA-U\ch-0\212.5457.46\bin" -Dfile.encoding=UTF-8 -classpath "C:\Users\Asus GL752\IdeaProjects\dzlesson7part2\out\production\dzlesson7part2" com.company.Main
    i = I
    i = v
    i = a
    i = n
    i = o
    i = v
    i = I
    i = v
    i = a
    i = n
    
    Process finished with exit code 0

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char[] reverseFullName = {'n', 'a', 'v', 'I', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length-1; i >= 0; i--){
+            System.out.println("i = " + reverseFullName[i]);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
